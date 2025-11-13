ab4067cd395b3d798fb838431ed20d3e7d645d0b
drichard989 <drichard989@gmail.com>
2025-11-12 23:13:40 -0800

    card divider improbem

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Golf Scorecard PWA
 // Update CACHE_VERSION every time you deploy changes
-const CACHE_VERSION = 'v1.0.3';
+const CACHE_VERSION = 'v1.0.4';
 const CACHE_NAME = `golf-${CACHE_VERSION}`;
 
 // Files to cache
