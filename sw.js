d4584869474f89009e814b1afc9e06b0dbacd53d
drichard989 <drichard989@gmail.com>
2026-04-28 12:13:46 -0700

    wefw
    efewf

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Golf Scorecard PWA
 // Update CACHE_VERSION every time you deploy changes
-const CACHE_VERSION = 'v3.3.276';
+const CACHE_VERSION = 'v3.3.278';
 const CACHE_NAME = `golf-${CACHE_VERSION}`;
 
 // Files to cache - comprehensive list
